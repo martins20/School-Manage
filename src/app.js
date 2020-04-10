35449ad53a13a1b7bca20c547655e7dbe76d430b
martins20 <pdesigner@gmail.com>
2020-04-10 09:30:18 -0300

    Add CORS and Helmet for more safely.

--- src/app.js
+++ src/app.js
@@ -6,8 +6,22 @@ dotenv.config();
 import "./database/connection";
 
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
 
 import routes from "./routes";
+
+const whiteList = ["http://localhost:3333"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("not allowed by CORS"));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -17,6 +31,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, "..", "temp", "uploads")));
