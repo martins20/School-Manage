c42a81736e93d3778fc04250e6afd1815c0db67d
martins20 <pdesigner@gmail.com>
2020-04-10 20:55:09 -0300

    Config Redux

--- Frontend/src/components/Header/index.js
+++ Frontend/src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import { Link } from 'react-router-dom';
-
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
-
 import { Nav } from './styles';
 
 export default function Header() {
+  const botaoClicado = useSelector((state) => state.example.botaoClicado);
+
   return (
     <Nav>
       <Link to="/">
@@ -18,6 +19,7 @@ export default function Header() {
       <Link to="/asdas">
         <FaUserAlt size={24} />
       </Link>
+      {botaoClicado ? 'Clicado' : 'Nao Clicado'}
     </Nav>
   );
 }
--- Frontend/src/pages/Login/index.js
+++ Frontend/src/pages/Login/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Title } from './styles';
 import { Container } from '../../styles/global';
+import * as exampleActions from '../../store/modules/example/actions';
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    dispatch(exampleActions.HandleButton());
+  }
   return (
     <Container>
       <Title>Pagina de Login</Title>
-      <button>Enviar</button>
+      <button onClick={handleClick}>Enviar</button>
     </Container>
   );
 }
