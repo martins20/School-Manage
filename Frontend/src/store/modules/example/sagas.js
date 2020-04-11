4ff1153cc7ad341eaa0da39838abbd61a8acfc8b
martins20 <pdesigner@gmail.com>
2020-04-10 22:02:34 -0300

    Fixed

--- Frontend/src/store/modules/example/sagas.js
+++ Frontend/src/store/modules/example/sagas.js
@@ -1,7 +1,7 @@
 import { call, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import * as actions from './actions';
-import * as types from '.././types';
+import * as types from '../types';
 
 const requisicao = () =>
   new Promise((resolve, reject) => {
