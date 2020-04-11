378ee5d4959bbaec887a1b1800d957c70996d488
martins20 <pdesigner@gmail.com>
2020-04-10 23:25:04 -0300

    Create StudentList page

--- Frontend/src/pages/StudentList/index.js
+++ Frontend/src/pages/StudentList/index.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { get } from 'lodash';
+import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
+
 import { Container } from '../../styles/global';
+import { UserContainer, ProfilePicture } from './styles';
+import axios from '../../services/api';
 
 export default function StudentList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function Data() {
+      const response = await axios.get('/students');
+      setUsers(response.data);
+    }
+
+    Data();
+  }, []);
   return (
     <Container>
-      <h1>Student List</h1>
+      <h1>Students</h1>
+      <UserContainer>
+        {users.map((user) => (
+          <div key={String(user.id)}>
+            <ProfilePicture>
+              {get(user, 'Photos[0].url', false) ? (
+                <img src={user.Photos[0].url} alt="" />
+              ) : (
+                <FaUserCircle size={36} />
+              )}
+            </ProfilePicture>
+            <span>{user.name}</span>
+            <span>{user.email}</span>
+
+            <Link to={`/user/${user.id}/edit`}>
+              <FaEdit size={16} />
+            </Link>
+            <Link to={`/user/${user.id}/delete`}>
+              <FaWindowClose size={16} />
+            </Link>
+          </div>
+        ))}
+      </UserContainer>
     </Container>
   );
 }
